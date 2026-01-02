f2227494bc5e37b269d44c2192cb51c4ea0c064f
rodhfr <souzafrodolfo@gmail.com>
2026-01-02 01:23:22 -0300

    day 9

--- day9-conditionals.py
+++ day9-conditionals.py
@@ -4,12 +4,12 @@ Get user input using input(“Enter your age: ”). If user is 18 or older, give
 You are old enough to drive. If below 18 give feedback to wait for the missing amount of years.
 """
 
-# age = input("Enter your age: ")
-# if int(age) >= 18:
-#    print("You are old enough to drive.")
-# else:
-#    r = 18 - int(age)
-#    print(f"Wait {r} year(s) to drive.")
+age = input("Enter your age: ")
+if int(age) >= 18:
+    print("You are old enough to drive.")
+else:
+    r = 18 - int(age)
+    print(f"Wait {r} year(s) to drive.")
 
 """
 Compare the values of my_age and your_age using if … else. Who is older (me or you)? 
@@ -18,15 +18,15 @@ print 'year' for 1 year difference in age, 'years' for bigger differences, and a
 text if my_age = your_age.
 """
 
-# your_age = int(input("Enter your age: "))
-# my_age = 28
-#
-# if your_age > my_age:
-#    print(f"You are {your_age - my_age} year(s) older than me.")
-# elif your_age < my_age:
-#    print(f"You are {my_age - your_age} year(s) newer than me")
-# else:
-#    print("You have the same age as me.")
+your_age = int(input("Enter your age: "))
+my_age = 28
+
+if your_age > my_age:
+    print(f"You are {your_age - my_age} year(s) older than me.")
+elif your_age < my_age:
+    print(f"You are {my_age - your_age} year(s) newer than me")
+else:
+    print("You have the same age as me.")
 
 
 """ 
@@ -34,14 +34,14 @@ Get two numbers from the user using input prompt. If a is greater than b return 
 is greater than b, if a is less b return a is smaller than b, else a is equal to b.
 """
 
-# n1 = input("Write me a number: ")
-# n2 = input("Ok. \nNow other number: ")
-# if n1 > n2:
-#     print(n1, "is greater than", n2)
-# elif n1 < n2:
-#     print(n2, "is greater than", n1)
-# else:
-#     print(n1, "is equal to", n2)
+n1 = input("Write me a number: ")
+n2 = input("Ok. \nNow other number: ")
+if n1 > n2:
+    print(n1, "is greater than", n2)
+elif n1 < n2:
+    print(n2, "is greater than", n1)
+else:
+    print(n1, "is equal to", n2)
 
 
 ## LEVEL 2
@@ -49,19 +49,19 @@ is greater than b, if a is less b return a is smaller than b, else a is equal to
 Write a code which gives grade to students according to theirs scores
 """
 
-# score = [10, 55, 65, 75, 85]
-# for s in score:
-#     if s >= 80 and s <= 100:
-#         s = "A"
-#     elif s >= 70 and s <= 89:
-#         s = "B"
-#     elif s >= 60 and s <= 69:
-#         s = "C"
-#     elif s >= 50 and s <= 59:
-#         s = "D"
-#     else:
-#         s = "F"
-#     print(s)
+score = [10, 55, 65, 75, 85]
+for s in score:
+    if s >= 80 and s <= 100:
+        s = "A"
+    elif s >= 70 and s <= 89:
+        s = "B"
+    elif s >= 60 and s <= 69:
+        s = "C"
+    elif s >= 50 and s <= 59:
+        s = "D"
+    else:
+        s = "F"
+    print(s)
 
 """
 Check if the season is Autumn, Winter, Spring or Summer. If the user input is: September, 
@@ -69,31 +69,32 @@ October or November, the season is Autumn. December, January or February, the se
 March, April or May, the season is Spring June, July or August, the season is Summer
 """
 
-# seasons = {
-#     "autumn": ["September", "October", "November"],
-#     "winter": ["December", "January", "February"],
-#     "spring": ["March", "April", "May"],
-#     "summer": ["June", "July", "August"],
-# }
-# usr_ssn = input("Which month are you on: ")
-# for season, months in seasons.items():
-#     for mes in months:
-#         if usr_ssn.lower() == mes.lower():
-#             print(season)
+seasons = {
+    "autumn": ["September", "October", "November"],
+    "winter": ["December", "January", "February"],
+    "spring": ["March", "April", "May"],
+    "summer": ["June", "July", "August"],
+}
+usr_ssn = input("Which month are you on: ")
+for season, months in seasons.items():
+    for mes in months:
+        if usr_ssn.lower() == mes.lower():
+            print(season)
 
 
-# fruits = ["banana", "orange", "mango", "lemon"]
+fruits = ["banana", "orange", "mango", "lemon"]
+
 """
 If a fruit doesn't exist in the list add the fruit to the list and print the modified list.
 If the fruit exists print('That fruit already exist in the list')
 """
-# usr_fruit = str(input("Write a fruit: "))
-# if usr_fruit not in fruits:
-#     fruits.append(usr_fruit)
-#     print("\nFruit Added to list:")
-# else:
-#     print(f"{usr_fruit} is already present.")
-# print(fruits)
+usr_fruit = str(input("Write a fruit: "))
+if usr_fruit not in fruits:
+    fruits.append(usr_fruit)
+    print("\nFruit Added to list:")
+else:
+    print(f"{usr_fruit} is already present.")
+print(fruits)
 
 ## LEVEL 3
 person = {
@@ -110,34 +111,34 @@ person = {
 Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
 """
 
-#
-# def middle_list(inpt_lst):
-#     mid = len(inpt_lst)
-#     print(mid)
-#     if mid % 2 == 1:
-#         mid = mid // 2
-#         print(inpt_lst[mid])
-#         return [inpt_lst[mid]]
-#     else:
-#         mid1 = mid // 2 - 1
-#         mid2 = mid // 2
-#         print([inpt_lst[mid1], inpt_lst[mid2]])
-#         return [inpt_lst[mid1], inpt_lst[mid2]]
-#
-#
-# prs_s = person.get("skills")
-# if prs_s:
-#     mid_prs_s = middle_list(prs_s)
-#
+
+def middle_list(inpt_lst):
+    mid = len(inpt_lst)
+    print(mid)
+    if mid % 2 == 1:
+        mid = mid // 2
+        print(inpt_lst[mid])
+        return [inpt_lst[mid]]
+    else:
+        mid1 = mid // 2 - 1
+        mid2 = mid // 2
+        print([inpt_lst[mid1], inpt_lst[mid2]])
+        return [inpt_lst[mid1], inpt_lst[mid2]]
+
+
+prs_s = person.get("skills")
+if prs_s:
+    mid_prs_s = middle_list(prs_s)
+
 """
 Check if the person dictionary has skills key, if so check if the person has 'Python' 
 skill and print out the result.
 """
-#
-# if prs_s:
-#     if "Python" in prs_s:
-#         print(prs_s)
-#         print(True)
+
+if prs_s:
+    if "Python" in prs_s:
+        print(prs_s)
+        print(True)
 
 """
 * If a person skills has only JavaScript and React, print('He is a front end developer'), 
